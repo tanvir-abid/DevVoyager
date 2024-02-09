3f452197c9f4dfdb7d9bc6a197083ebb98bd8666
tanvir-abid <118760910+tanvir-abid@users.noreply.github.com>
2024-02-10 00:34:28 +0600

    elements initiate

--- js/app.js
+++ js/app.js
@@ -3,21 +3,20 @@ document.addEventListener('DOMContentLoaded', async () => {
     generateMetaTags(data.metaTags);
     let container = document.querySelector('main');
     // typewriterWords 
-    // const loadingWords = ['The Best Web Developer. ','Loading...', 'The Best SEO Expert In Bangladesh. '];
-    // let loadingAnimationContainer = createLoadingAnimation(data['section-home'].ownerInfo.name, data.metaTags.keywords,loadingWords);
-    // container.append(loadingAnimationContainer);
+    const loadingWords = ['The Best Web Developer. ','Loading...', 'The Best SEO Expert In Bangladesh. '];
+    let loadingAnimationContainer = createLoadingAnimation(data['section-home'].ownerInfo.name, data.metaTags.keywords,loadingWords);
+    container.append(loadingAnimationContainer);
 
-    // // Calculate the total duration based on the length of typewriter words
-    // const totalDuration = loadingWords.reduce((acc, word) => acc + word.length, 0) * 61.2;
+    // Calculate the total duration based on the length of typewriter words
+    const totalDuration = loadingWords.reduce((acc, word) => acc + word.length, 0) * 61.2;
 
-    // setTimeout(() => {
-    //     loadingAnimationContainer.classList.add('active');
-    //     setTimeout(()=>{
-    //         loadingAnimationContainer.remove();
-    //         getDataAndGenrateInitialElements();
-    //     },300);
-    // }, totalDuration);
-    getDataAndGenrateInitialElements();
+    setTimeout(() => {
+        loadingAnimationContainer.classList.add('active');
+        setTimeout(()=>{
+            loadingAnimationContainer.remove();
+            getDataAndGenrateInitialElements();
+        },300);
+    }, totalDuration);
 });
 
 
@@ -263,7 +262,7 @@ async function getDataAndGenrateInitialElements(){
     document.querySelector('main').appendChild(menubar);
     //---------------------------------------------//
 
-    // createSlideshow(alldata["section-home"].slides);
+    createSlideshow(alldata["section-home"].slides);
     //createAboutContainer(alldata['section-about'].texts);
     // createServicesSection(alldata['section-services'].services);
     // createPortfolioSection(alldata['section-portfolio'].projects);
